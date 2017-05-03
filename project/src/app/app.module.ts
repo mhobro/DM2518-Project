e16880c3b6aeac27e8decd9c8690d28e3bd61609
Mark Hobro <mhobro@kth.se>
2017-05-03 21:28:42 +0200

    Still getting some errors when running the app

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,45 +1,55 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
-import { ItemDetailsPage } from '../pages/item-details/item-details';
-import { ListPage } from '../pages/list/list';
+import {AngularFireModule} from 'angularfire2';
+
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-// Import the AF2 Module
-import { AngularFireModule } from 'angularfire2';
 
-// AF2 Settings
-export const firebaseConfig = {
-  apiKey: "AIzaSyByAfaZFGNWCdgT30WegtVIBAwfrjzAi0c",
-  authDomain: "dm2518-project-9da4c.firebaseapp.com",
-  databaseURL: "https://dm2518-project-9da4c.firebaseio.com",
-  storageBucket: "dm2518-project-9da4c.appspot.com",
-  messagingSenderId: "814669340370"
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyByAfaZFGNWCdgT30WegtVIBAwfrjzAi0c",
+    authDomain: "dm2518-project-9da4c.firebaseapp.com",
+    databaseURL: "https://dm2518-project-9da4c.firebaseio.com",
+    projectId: "dm2518-project-9da4c",
+    storageBucket: "dm2518-project-9da4c.appspot.com",
+    messagingSenderId: "814669340370"
 };
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
