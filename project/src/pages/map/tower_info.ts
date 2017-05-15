c068bb6fe569c9b09e3cb387ce71dc3d1736d7c0
Yann Lachever <lachevery@gmail.com>
2017-05-15 23:22:30 +0200

    Design done

--- project/src/pages/map/tower_info.ts
+++ project/src/pages/map/tower_info.ts
@@ -7,22 +7,28 @@ import {Tower} from './tower';
   selector: 'tower-detail',
   template: `
     <div *ngIf="data">
-      <ion-toolbar>
-        <ion-title><h2>Tower</h2></ion-title>
+      <ion-toolbar color="orange">
+        <ion-title style="color:white; font-weight: bold;"><h2>Tower</h2></ion-title>
       </ion-toolbar>
       <div *ngIf="data.tower.activated">
-        <div>
+        <div style="margin: 20px auto;">
+          <img width="200" src="{{data.tower.img}}"/>
+        </div>
+        <div id="towerTitle">
           <h1>{{data.tower.name}}</h1>
+        </div>
           <p><label>Congratulation !</label></p>
           <p><label>You unlocked the area {{data.tower.name}}.</label></p>
-        </div>
       </div>
       <div *ngIf="!data.tower.activated">
+        <div style="margin: 20px auto;">
+          <img width="75" height="75" src="assets/marker/tower_icon.svg"/>
+        </div>
         <label>Visit the tower to unlock the area.</label>
-        <div>
+        <div id="towerTitle" class="text_grey">
           <h1>{{data.tower.name}}</h1>
         </div>
-        <button ion-button round block class="btn btn-success" (click)="unlockTower();" [disabled]="!data.unlockable">
+        <button ion-button round color="secondary" class="btn btn-success" (click)="unlockTower();" [disabled]="!data.unlockable">
           Unlock
         </button>
         <div *ngIf="!data.unlockable">
